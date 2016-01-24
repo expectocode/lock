b0da9f3d635a3afba11c7c674e31f3123e634201
Tanuj <expectocode@users.noreply.github.com>
2016-01-24 17:35:03 +0000

    Updated generator snapshot:  - better random number generater  - multi line quotes

--- quotesCategoriesSnapshot24011026.cpp
+++ quotesCategoriesSnapshot24011026.cpp
@@ -4,10 +4,8 @@
 using namespace std;
 int main()
 {
-    //"FIRE! FIRE! HELP ME! 123 Cavendon Road. Looking forward to hearing from you. Yours truly, Maurice Moss",
-    //was too long
     srand (time(NULL));
-    char quoteMark = char(34);
+    //char quoteMark = char(34);
     //codes: h hacker i itcrowd s starwars p potter l sherlock m memes/misc
     vector <string> quotes = {"hYou can't synthesize the application without hacking the haptic database!",
                               "hWe need to reboot the auxiliary HDD sensor!",
@@ -15,9 +13,11 @@ int main()
                               "hUse the open-source SAS sensor, then you can navigate the digital driver!",
                               "hUse the virtual IB hard drive, then you can hack the transmitting bus!",
                               "hI'll reboot the solid state bluetooth system, that should connect the GB sensor",
+                              "If we generate the panel, we can get to the firewall transmitter \n through the wireless XSS interface!",
                               "iI'll just put it here with the rest of the fire.",
                               "i0118 999 881 999 119 725... 3",
                               "iHello, IT. Have you tried turning it off and on again? ",
+                              "FIRE! FIRE! HELP ME!\n 123 Cavendon Road. Looking forward to hearing from you.\n Yours truly, Maurice Moss",
                               "sThat's not how the Force works!",
                               "sNo. I am your Father.",
                               "sI find your lack of faith disturbing.",
@@ -40,10 +40,13 @@ int main()
                               "lAnderson, don’t talk out loud. You lower the IQ of the whole street.",
                               "lEvery fairy tale needs a good old-fashioned villain...",
                               "lI’m in shock. Look - I’ve got a blanket.",
-                              "lNo, it’s the burglar, he’s got himself rather badly injured. He fell out of a window.",
+                              "lNo, it’s the burglar, he’s got himself rather badly injured.\n He fell out of a window.",
                               "lHoney, you should see me in a crown.",
 
                              };
-    string quoteChoice = quotes[rand() % quotes.size()];
+    //string quoteChoice = quotes[rand() % quotes.size()];
+    int randomNumber = static_cast<double>(std::rand()) / RAND_MAX * quotes.size();
+    //cout<<"Random Number: "<<randomNumber<<endl;
+    string quoteChoice = quotes[randomNumber];
     cout<<quoteChoice<<endl;
 }
